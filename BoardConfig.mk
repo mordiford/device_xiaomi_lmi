bf2b50695d26deac72e21bcdfa186d9ac3ced23f
Christopher N. Hesse <raymanfx@gmail.com>
2017-04-18 09:22:49 +0200

    lmi: Enforce vendor tree presence
    A compiled build has no chance of booting without the prebuilt
    binaries, so fail the build if those are not present.
    
    Change-Id: Iadd43e887d426732e549969ab8deb14194ca818d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,4 +30,4 @@ TARGET_KERNEL_CONFIG := vendor/lmi_defconfig
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 
 # inherit from the proprietary version
--include vendor/xiaomi/lmi/BoardConfigVendor.mk
+include vendor/xiaomi/lmi/BoardConfigVendor.mk
