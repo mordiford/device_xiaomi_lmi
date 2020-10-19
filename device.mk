7f03f20b8dfb44cd7c649ff1c7f3baa8947d3d51
lindwurm <lindwurm.q@gmail.com>
2020-10-20 06:11:12 +0900

    Revert "lmi: rename overlay-lineage to overlay-aicp"
    This reverts commit 6563d9858bdb143eddaeae2484381c0254556ca2.

--- device.mk
+++ device.mk
@@ -126,7 +126,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aicp
+    $(LOCAL_PATH)/overlay-lineage
 
 # Permissions
 PRODUCT_COPY_FILES += \
