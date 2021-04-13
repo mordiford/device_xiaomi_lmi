dbbba79e0ac4b5e1bbb0e00f37f58eb6827bbaaf
lindwurm <lindwurm.q@gmail.com>
2021-04-13 15:41:11 +0900

    lmi: TARGET_HAS_FOD
    Signed-off-by: lindwurm <lindwurm.q@gmail.com>

--- lineage_lmi.mk
+++ lineage_lmi.mk
@@ -14,6 +14,11 @@ $(call inherit-product, device/xiaomi/lmi/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# FOD
+TARGET_HAS_FOD =: true
+EXTRA_FOD_ANIMATIONS := true
+
+# Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_lmi
 PRODUCT_DEVICE := lmi
 PRODUCT_MANUFACTURER := Xiaomi
